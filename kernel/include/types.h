eea569577d3241b9d6f492b6dcd005547dfb4f9f
Ethan88438 <eth88438@gmail.com>
2026-09-12 11:25:45 +0000

    Final.

---
+++ kernel/include/types.h
@@ -0,0 +1,13 @@
+#ifndef KERNEL_TYPES_H
+#define KERNEL_TYPES_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif
