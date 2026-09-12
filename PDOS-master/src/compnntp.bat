758262352338730ed1d774bb60b1d898c4a97c91
Ethan88438 <eth88438@gmail.com>
2026-09-12 08:53:07 +0000

    Removed unnecessary files and added new components for PDOS-master, including BIOS, network, Pascal, PCI, PDCC, PDPMake, PDPCLib, S370, and UDI directories with their respective source files and headers. Literally all of it.

--- PDOS-master/src/compnntp.bat
+++
@@ -1,5 +0,0 @@
-gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ pdpnntp.c
-aswin -o pdpnntp.o pdpnntp.s
-del pdpnntp.s
-
-ldwin -o pdpnntp.exe ../pdpclib/w32start.o pdpnntp.o ../pdpclib/msvcrt.a
