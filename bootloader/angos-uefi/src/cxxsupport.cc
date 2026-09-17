dc59c50fa37a0b48836f883a3e9bb396ec4ac143
Isan88438 <eth88438@gmail.com>
2026-09-17 20:41:52 +1000

    Modification 7G

--- bootloader/angos-uefi/src/cxxsupport.cc
+++ bootloader/angos-uefi/src/cxxsupport.cc
@@ -31,3 +31,43 @@ extern "C"
 void free(void *v) {
     return free_pool(v);
 }
+
+
+extern "C"
+int memcmp(const void *a, const void *b, size_t n)
+{
+    const unsigned char *aa = (const unsigned char *)a;
+    const unsigned char *bb = (const unsigned char *)b;
+
+    for (size_t i = 0; i < n; ++i) {
+        if (aa[i] < bb[i])
+            return -1;
+        if (aa[i] > bb[i])
+            return 1;
+    }
+
+    return 0;
+}
+
+extern "C"
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char *d = (unsigned char *)dest;
+    const unsigned char *s = (const unsigned char *)src;
+
+    for (size_t i = 0; i < n; ++i)
+        d[i] = s[i];
+
+    return dest;
+}
+
+extern "C"
+void *memset(void *dest, int value, size_t n)
+{
+    unsigned char *d = (unsigned char *)dest;
+
+    for (size_t i = 0; i < n; ++i)
+        d[i] = (unsigned char)value;
+
+    return dest;
+}
