b98bf17965fd4a3a1c0ff58a0253253ce797417d
Isan88438 <eth88438@gmail.com>
2026-09-19 22:22:35 +1000

    Still fixing a fatal error in memory.h

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -2,6 +2,7 @@
 #define MEMORY_H
 #include <types.h>
 #include <tosaithe.h>
+#include <tosaithe.h>
 
 #define PAGE_SIZE 4096
 
@@ -30,8 +31,7 @@ void *find_physaddr(void *virt);
 void *phys_page_alloc();
 void phys_page_free(void *addr);
 
-struct tosaithe_loader_data;
-void phys_memory_init(const struct tosaithe_loader_data *loader_data);
+void phys_memory_init(const tosaithe_loader_data *loader_data);
 u64 phys_memory_total_pages(void);
 u64 phys_memory_free_pages(void);
 
