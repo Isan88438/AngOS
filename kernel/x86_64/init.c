b6bc480793e6672c84aa253ad76125cd14af662d
Isan88438 <eth88438@gmail.com>
2026-09-20 15:06:39 +1000

    Placed the global definitions back into init.c

--- kernel/x86_64/init.c
+++ kernel/x86_64/init.c
@@ -10,6 +10,7 @@
 #include <tosaithe.h>
 #include <terminal.h>
 #include <globals.h>
+#include "init.h"
 
 void init(tosaithe_loader_data *loader_data) {
         if (!loader_data || !loader_data->kern_map) {
