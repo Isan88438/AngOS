0509ffe2931ff1de1a7a70047b75cf8fec2f6484
Isan88438 <eth88438@gmail.com>
2026-09-20 14:28:50 +1000

    Updated the paging header.

--- kernel/x86_64/memory/paging.h
+++ kernel/x86_64/memory/paging.h
@@ -1,16 +1,27 @@
 #ifndef PAGING_H
 #define PAGING_H
+
 #include <types.h>
 #include <memory.h>
 #include <globals.h>
 
-#define FLAG_PRESENT (1 << 0)
-#define FLAG_RW (1 << 1)
-#define FLAG_US (1 << 2)
-#define FLAGS (FLAG_PRESENT | FLAG_RW | FLAG_US)
-#define ADDR_MASK 0x000ffffffffff000
+#define FLAG_PRESENT (1ULL << 0)
+#define FLAG_RW      (1ULL << 1)
+#define FLAG_US      (1ULL << 2)
 
-struct table pml4;
+#define FLAGS        (FLAG_PRESENT | FLAG_RW | FLAG_US)
+#define ADDR_MASK    0x000ffffffffff000ULL
+
+struct table {
+        u64 entry[512];
+} __attribute__((aligned(4096)));
+
+extern struct table pml4;
+
+void map_page(void *phys, void *virt);
+void map_2mb_page(void *phys, void *virt);
+void *find_physaddr(void *virt);
+void init_paging(void);
 
 extern void load_pml4(struct table *pml4);
 
