803d60244410162d15477309e9201410bea1c809
Isan88438 <eth88438@gmail.com>
2026-09-23 16:19:49 +1000

    Added a new bit to the end of entry.c

--- kernel/x86_64/entry.c
+++ kernel/x86_64/entry.c
@@ -21,3 +21,41 @@ void angos_entry(tosaithe_loader_data *loader_data) {
         asm volatile ("hlt");
     }
 }
+
+[bits 64]
+global irq1_stub
+extern keyboard_handler
+
+irq1_stub:
+    push rbx
+    push rcx
+    push rdx
+    push rbp
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    call keyboard_handler
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rsi
+    pop rdi
+    pop rbp
+    pop rdx
+    pop rcx
+    pop rbx
+    iretq
