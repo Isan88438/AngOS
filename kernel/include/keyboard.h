2b448ff123e870b150324ca49ac74a0223628a0d
Isan88438 <eth88438@gmail.com>
2026-09-23 16:14:14 +1000

    Updated the header file with PS/2 constants, scan code declarations, and function prototypes.

--- kernel/include/keyboard.h
+++ kernel/include/keyboard.h
@@ -1,6 +1,13 @@
-#ifndef ANGOS_KEYBOARD_H
-#define ANGOS_KEYBOARD_H
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <types.h>
+
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
 
 void keyboard_init(void);
+void keyboard_handler(void);
+char scancode_to_ascii(uint8_t scancode);
 
 #endif
