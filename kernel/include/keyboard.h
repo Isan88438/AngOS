3211f5a8d3c9c909e7a99dd1156e24ea25b5c8db
Isan88438 <eth88438@gmail.com>
2026-09-23 16:28:16 +1000

    Added a Keyboard Ring Buffer.

--- kernel/include/keyboard.h
+++ kernel/include/keyboard.h
@@ -2,12 +2,17 @@
 #define KEYBOARD_H
 
 #include <types.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_DATA_PORT   0x60
 #define KEYBOARD_STATUS_PORT 0x64
+#define KBD_BUFFER_SIZE      256
 
 void keyboard_init(void);
 void keyboard_handler(void);
-char scancode_to_ascii(uint8_t scancode);
+bool keyboard_has_char(void);
+char keyboard_getchar(void);
+void keyboard_gets(char *buf, size_t max_len);
 
 #endif
