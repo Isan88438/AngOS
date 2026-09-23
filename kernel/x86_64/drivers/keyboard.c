538da4c2259ea95754710877618bb8c8e8a8f850
Isan88438 <eth88438@gmail.com>
2026-09-23 16:16:04 +1000

    Rehauled keyboard.c again. This driver reads Scan Code Set 1 from port 0x60, tracks Shift and Caps Lock state, maps keypresses to ASCII, prints characters to the screen, and sends End-Of-Interrupt (EOI) to the PIC.

--- kernel/x86_64/drivers/keyboard.c
+++ kernel/x86_64/drivers/keyboard.c
@@ -1,82 +1,55 @@
-#include <types.h>
-#include <stdbool.h>
-#include <cpu/IO.h>
-#include <cpu/IDT.h>
-#include <terminal.h>
 #include <keyboard.h>
+#include <cpu/IO.h>
+#include <terminal.h>
 
-extern void piceoi(bool isslave);
+static bool shift_pressed = false;
+static bool caps_lock = false;
 
-static const char keymap[128] = {
-    [0x02] = '1',
-    [0x03] = '2',
-    [0x04] = '3',
-    [0x05] = '4',
-    [0x06] = '5',
-    [0x07] = '6',
-    [0x08] = '7',
-    [0x09] = '8',
-    [0x0A] = '9',
-    [0x0B] = '0',
-
-    [0x10] = 'q',
-    [0x11] = 'w',
-    [0x12] = 'e',
-    [0x13] = 'r',
-    [0x14] = 't',
-    [0x15] = 'y',
-    [0x16] = 'u',
-    [0x17] = 'i',
-    [0x18] = 'o',
-    [0x19] = 'p',
-
-    [0x1E] = 'a',
-    [0x1F] = 's',
-    [0x20] = 'd',
-    [0x21] = 'f',
-    [0x22] = 'g',
-    [0x23] = 'h',
-    [0x24] = 'j',
-    [0x25] = 'k',
-    [0x26] = 'l',
-
-    [0x2C] = 'z',
-    [0x2D] = 'x',
-    [0x2E] = 'c',
-    [0x2F] = 'v',
-    [0x30] = 'b',
-    [0x31] = 'n',
-    [0x32] = 'm',
-
-    [0x39] = ' ',
+static const char scancode_map[] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', ' = ', '\b',
+  '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+     0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+     0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ', ', '.', '/', 0,
+   '*',   0, ' '
 };
 
-static void keyboard_interrupt(void)
-{
-    u8 scancode = inb(0x60);
+static const char scancode_map_shift[] = {
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
+  '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',
+     0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',
+     0, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0,
+   '*',   0, ' '
+};
 
-    /*
-     * Ignore key-release scancodes.
-     */
-    if (!(scancode & 0x80)) {
-        if (scancode < 128) {
-            char c = keymap[scancode];
+void keyboard_init(void) {
 
-            if (c != 0)
-                terminal_putchar(c);
+    uint8_t mask = inb(0x21);
+    outb(0x21, mask & ~(1 << 1));
+}
+
+void keyboard_handler(void) {
+    uint8_t scancode = inb(KEYBOARD_DATA_PORT);
+
+    if (scancode & 0x80) {
+        uint8_t released_code = scancode & 0x7F;
+        if (released_code == 0x2A || released_code == 0x36) {
+            shift_pressed = false;
+        }
+    } else {
+
+        if (scancode == 0x2A || scancode == 0x36) {
+            shift_pressed = true;
+        } else if (scancode == 0x3A) {
+            caps_lock = !caps_lock;
+        } else if (scancode < sizeof(scancode_map)) {
+            bool use_shift = shift_pressed ^ caps_lock;
+            char ch = use_shift ? scancode_map_shift[scancode] : scancode_map[scancode];
+
+            if (ch != 0) {
+                terminal_putchar(ch);
+            }
         }
     }
 
-    /*
-     * IRQ1 came from the master PIC.
-     */
-    piceoi(false);
-}
-
-void keyboard_init(void)
-{
-    addirq(0x21, (void *)keyboard_interrupt, 0x8E);
-    unmaskirq(1);
-
-    terminal_write("KEYBOARD OK\n");
+    outb(0x20, 0x20);
 }
