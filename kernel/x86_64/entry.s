cae0939aff12b85b59b53a470e3208bca863ef19
Isan88438 <eth88438@gmail.com>
2026-09-24 18:50:01 +1000

    Replaced entry.s

--- kernel/x86_64/entry.s
+++ kernel/x86_64/entry.s
@@ -1,9 +1,26 @@
+# Tosaithe boot protocol header
+.section .tsbp,"a"
+
+.global angos_tosaithe_header
+
+angos_tosaithe_header:
+    .long 0x50425354      # "TSBP"
+    .long 1               # protocol version
+    .long 1               # minimum required version
+    .long 1               # requires framebuffer
+    .quad angos_stack_top
+
+# Kernel entry point
 .section .text.entry
+
 .global angos_entry
+.extern init
+.extern kernel_main
 
 angos_entry:
     call init
-    cli
+    call kernel_main
+
 1:
     hlt
     jmp 1b
